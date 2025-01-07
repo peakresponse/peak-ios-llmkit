f5711d7e909d2ba42c15e41ff97634b88951d802
Francis Li <mail@francisli.com>
2025-01-07 14:33:15 -0800

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "LLMKit",
+    name: "peak-ios-llmkit",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LLMKit",
-            targets: ["LLMKit"]),
+            targets: ["LLMKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/eastriverlee/LLM.swift", branch: "pinned")
@@ -18,13 +18,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LLMKit"
+            name: "LLMKit",
             dependencies: [
-                .product(name: "LLM", package: "LLM")
+                .product(name: "LLM", package: "LLM.swift")
             ]),
         .testTarget(
             name: "LLMKitTests",
-            dependencies: ["LLMKit"]
-        ),
+            dependencies: ["LLMKit"])
     ]
 )
